c87ebd012f62d32ec8f26aa2201e0648839699da
Oliwier Perzyński <olek@oliverperzyk.com>
2026-08-29 16:05:39 +0200

    feat(dev): added setup for development (db); added activity for a bot

--- src/events/client/ClientReadyEventListener.ts
+++ src/events/client/ClientReadyEventListener.ts
@@ -1,4 +1,4 @@
-import { type Client, Events } from "discord.js"
+import { type Client, Events, ActivityType } from "discord.js"
 import { BaseEventListener } from "../base/BaseEventListener"
 import { SlashCommandRegistry } from "@/oliverperzyk/commands/base/SlashCommandRegistry"
 
@@ -27,7 +27,11 @@ class ClientReadyEventListener extends BaseEventListener<Events.ClientReady> {
      * @summary Listener's behavior.
      * @description Method is triggered once the client is ready.
      */
-    public override async onceEvent(_client: Client<true>): Promise<void> {
+    public override async onceEvent(client: Client<true>): Promise<void> {
+        await client.user.setActivity({
+            name: "🍪 eating cookies...",
+            type: ActivityType.Custom,
+        })
         await SlashCommandRegistry.registerCommands()
     }
 }
